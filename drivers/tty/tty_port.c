1ad165a6a92cfeffa438b84eed819fe4de1a17e3
Camden Bruce <camden.o.b@gmail.com>
2023-05-11 12:18:19 +1200

    Revert tty_port
    Change-Id: I70990ab88705becb9fb8c6a42aa848bb07b11e29

--- drivers/tty/tty_port.c
+++ drivers/tty/tty_port.c
@@ -629,4 +629,3 @@ int tty_port_set_policy(struct tty_port *port, int policy, int sched_priority)
 	return sched_setscheduler(port->worker_thread, policy, &param);
 }
 EXPORT_SYMBOL_GPL(tty_port_set_policy);
-
